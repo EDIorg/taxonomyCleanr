ed83f951c6f7b542d29a77a7c152d275074e2648
Colin Smith <colin.smith@wisc.edu>
2018-10-23 17:05:32 -0500

    Allow vector input

--- R/trim_taxa.R
+++ R/trim_taxa.R
@@ -5,12 +5,14 @@
 #'     authority reduces the frequency of mismatches.
 #'
 #' @usage
-#'     trim_raw_taxa_str(path)
+#'     trim_raw_taxa_str(path = NULL, x = NULL)
 #'
 #' @param path
 #'     A character string specifying the path to taxa_map.csv. This table
 #'     tracks relationships between your raw and cleaned data and is operated
 #'     on by this function.
+#' @param x
+#'     (character) A vector of taxa to be trimmed.
 #'
 #' @details
 #'     List of conditions `trim_taxa` addresses:
@@ -30,78 +32,103 @@
 #'
 
 
-trim_taxa <- function(path){
+trim_taxa <- function(path = NULL, x = NULL){
 
 
 # Check arguments ---------------------------------------------------------
 
-  if (missing(path)){
-    stop('Input argument "path" is missing!')
-  }
-
-  validate_path(path)
-
-  use_i <- file.exists(
-    paste0(
-      path,
-      '/taxa_map.csv'
+  if (!is.null(path)){
+    EDIutils::validate_path(path)
+    use_i <- file.exists(
+      paste0(
+        path,
+        '/taxa_map.csv'
       )
     )
-  if (!isTRUE(use_i)){
-    stop('taxa_map.csv is missing! Create it with initialize_taxa_map.R.')
+    if (!isTRUE(use_i)){
+      stop('taxa_map.csv is missing! Create it with initialize_taxa_map.R.')
+    }
   }
+
+  if (!is.null(x)){
+    if (!is.character(x)){
+      stop('Input argument "x" must be a character vector.')
+    }
+  }
+
 
 # Read taxa_map.csv -------------------------------------------------------
 
-  x <- read.table(
-    paste0(
-      path,
-      '/taxa_map.csv'
+  if (!is.null(path)){
+    x <- read.table(
+      paste0(
+        path,
+        '/taxa_map.csv'
       ),
-    header = T,
-    sep = ',',
-    stringsAsFactors = F
+      header = T,
+      sep = ',',
+      stringsAsFactors = F
     )
+  }
 
   # Replace underscores with blank spaces -------------------------------------
 
-  x[ , 'taxa_trimmed'] <- str_replace_all(
-    x[ , 'taxa_raw'],
-    '_',
-    ' '
-  )
+  if (!is.null(path)){
+    x[ , 'taxa_trimmed'] <- str_replace_all(
+      x[ , 'taxa_raw'],
+      '_',
+      ' '
+    )
+  } else {
+    x <- str_replace_all(x, '_', ' ')
+  }
 
 # Trim white space --------------------------------------------------------
 
-  x[ , 'taxa_trimmed'] <- str_trim(
-    x[ , 'taxa_trimmed'],
-    'both'
+
+  if (!is.null(path)){
+    x[ , 'taxa_trimmed'] <- str_trim(
+      x[ , 'taxa_trimmed'],
+      'both'
     )
+  } else {
+    x <- str_trim(x, 'both')
+  }
 
 # Remove common species abbreviations at the genus level ------------------
 
-  x[ , 'taxa_trimmed'] <- str_remove(
-    x[ , 'taxa_trimmed'],
-    paste0(
-      '[:space:]+(?i)[s|c]+(\\.*[f|p]*\\.)+$',
-      '|[:space:]+(?i)[s|c]+([f|p]*\\.)+$',
-      '|[:space:]+(?i)[s|c]+([f|p]*)+$',
-      '|[:space:]+(?i)[s|c]+(\\.*[f|p]*)+$')
-  )
+  if (!is.null(path)){
+    x[ , 'taxa_trimmed'] <- str_remove(
+      x[ , 'taxa_trimmed'],
+      paste0(
+        '[:space:]+(?i)[s|c]+(\\.*[f|p]*\\.)+$',
+        '|[:space:]+(?i)[s|c]+([f|p]*\\.)+$',
+        '|[:space:]+(?i)[s|c]+([f|p]*)+$',
+        '|[:space:]+(?i)[s|c]+(\\.*[f|p]*)+$')
+    )
+  } else {
+    x <- str_remove(
+      x,
+      paste0(
+        '[:space:]+(?i)[s|c]+(\\.*[f|p]*\\.)+$',
+        '|[:space:]+(?i)[s|c]+([f|p]*\\.)+$',
+        '|[:space:]+(?i)[s|c]+([f|p]*)+$',
+        '|[:space:]+(?i)[s|c]+(\\.*[f|p]*)+$')
+      )
+  }
 
 # Document provenance -----------------------------------------------------
 
-  # Only list taxa names in taxa_trimmed that are different than taxa_raw
-
-  use_i <- x[ , 'taxa_raw'] == x[ , 'taxa_trimmed']
-  x[ , 'taxa_trimmed'][use_i] <- NA_character_
-
-  # Write to file
-
-  write_taxa_map(
-    x = x,
-    path = path
+  if (!is.null(path)){
+    # Only list taxa names in taxa_trimmed that are different than taxa_raw
+    use_i <- x[ , 'taxa_raw'] == x[ , 'taxa_trimmed']
+    x[ , 'taxa_trimmed'][use_i] <- NA_character_
+    # Write to file
+    write_taxa_map(
+      x = x,
+      path = path
     )
+  }
 
 # Return output -----------------------------------------------------------
 
