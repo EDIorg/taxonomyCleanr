cf2abd2c5734e8e68d71517747530ad285f90bba
clnsmth <colin.smith@wisc.edu>
2018-12-15 17:56:59 -0500

    Expand test coverage

--- tests/testthat/test_count_taxa.R
+++ tests/testthat/test_count_taxa.R
@@ -6,28 +6,35 @@ library(taxonomyCleanr)
 data <- utils::read.table(
   system.file('test_data.txt', package = 'taxonomyCleanr'),
   header = TRUE,
-  sep = '\t'
+  sep = '\t',
+  as.is = T
 )
 
-counts <- count_taxa(
-  x = data,
-  col = 'Species'
-)
+counts <- count_taxa(x = data, col = 'Species')
+
+path <- system.file('test_data.txt', package = 'taxonomyCleanr')
+path <- substr(path, 1, nchar(path) - 14)
 
 # Tests -----------------------------------------------------------------------
 
+# Generate errors
+testthat::test_that('Expect errors', {
+  expect_error(count_taxa(col = 'Species'))
+  #expect_error(count_taxa(x = c(1,2,3,4,5), col = 'Species'))
+  expect_error(count_taxa(x = data))
+  expect_error(count_taxa(x = data, col = 'Species', path = path))
+})
+
+# Input argument "x" is a data frame
 testthat::test_that('Output data.frame', {
+  expect_equal(class(counts), 'data.frame')
+  expect_equal(dim(counts), c(length(unique(data$Species)), ncol(counts)))
+})
 
-  expect_equal(
-    class(counts),
-    'data.frame'
-  )
-
-  expect_equal(
-    dim(counts),
-    c(length(unique(data$Species)), ncol(counts))
-  )
-
+# Input argument "x" is a vector of character strings
+testthat::test_that('Input is vector of character strings', {
+  expect_equal(nrow(count_taxa(x = data$Species)),
+               length(unique(data$Species)))
 })
 
 
