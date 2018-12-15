12db4a0169ddb04cd2dabf5721a20a728d42acd8
clnsmth <colin.smith@wisc.edu>
2018-12-15 17:24:13 -0500

    Refactor function

--- R/get_classification.R
+++ R/get_classification.R
@@ -1,33 +1,48 @@
-#' Get taxa classification
+#' Get taxa classification hierarchy
 #'
 #' @description
-#'     Get the classification hierarchy of a taxa
+#'     Get the classification hierarchy of taxa.
 #'
 #' @usage
-#'     get_classification(x, col = NULL, path = NULL)
+#'     get_classification(taxa.clean, authority, authority.id, path = NULL)
 #'
+#' @taxa.clean
+#'     (character) Name of taxa resolved to an authority.
 #' @param authority
 #'     (character) Vector of authorities corresponding to taxonomic IDs listed
 #'     in the "authority.id" argument below. Get data.sources and IDs with
 #'     `resolve_sci_taxa`.
 #' @param authority.id
 #'     (character) Vector taxonomic IDs corresponding to an authority.
+#' @param path
+#'     A character string specifying the path to taxa_map.csv. This table
+#'     tracks relationships between your raw and cleaned data and is operated
+#'     on by this function. Create this file with `create_taxa_map`.
 #'
 #' @return
-#'     (list) A list of hierarchical ranks for each taxa represented by an
-#'     authority.id
+#'     (list) A list taxa with corresponding hierarchical names and ranks.
 #'
 #' @export
 #'
 
-get_classification <- function(authority, authority.id){
+get_classification <- function(taxa.clean, authority, authority.id, path = NULL){
 
-  # Validate arguments --------------------------------------------------------
+  cw <- data.frame(human.readable = c('Catalogue of Life', 'ITIS',
+                                      'World Register of Marine Species',
+                                      'GBIF Backbone Taxonomy',
+                                      'Tropicos - Missouri Botanical Garden'),
+                   machine.readable = c('col', 'itis', 'worms', 'gbif',
+                                        'tropicos'),
+                   stringsAsFactors = F)
 
-  # resolve_sci_taxa ----------------------------------------------------------
-
-  mapply(taxize::classification, x = authority.id, db = authority)
-
-  #
+  if (is.null(path)){
+    use_i <- match(authority, cw$human.readable)
+    output <- suppressMessages(mapply(taxize::classification,
+                                      x = authority.id[use_i],
+                                      db = cw$machine.readable[use_i]))
+    names(output) <- taxa.clean[use_i]
+    output <- lapply(output, as.data.frame)
+    output
+  }
 
 }
