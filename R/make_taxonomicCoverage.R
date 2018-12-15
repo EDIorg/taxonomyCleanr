6a8c7b3e2ad46d4278d985b86e18886e29086baa
clnsmth <colin.smith@wisc.edu>
2018-12-15 17:24:45 -0500

    Release make_taxonomicCoverage.R

--- R/make_taxonomicCoverage.R
+++ R/make_taxonomicCoverage.R
@@ -1,98 +1,53 @@
-#' Make the taxonomicCoverage EML element
+#' Make taxonomicCoverage EML
 #'
 #' @description
-#'     Make the taxonomicCoverage EML element from information contained in
-#'     the file taxon.txt.
+#'     Make the hierarchical taxonomicCoverage EML node set.
 #'
 #' @usage
-#'     make_taxonomicCoverage(path = "")
+#'     make_taxonomicCoverage(taxa.clean, authority, authority.id, path = NULL)
 #'
+#' @param taxa.clean
+#'     (character) Taxa names that have been resolved to a taxonomic authority.
+#' @param authority
+#'     (character) Authorities to which the taxa have been resolved. Valid
+#'     inputs are created by `resolve_sci_taxa`.
+#' @param authority.id
+#'     (character) Taxonomic IDs corresponding the authority to which taxa
+#'     have been resolved.
 #' @param path
-#'     A path of the directory containing the file taxon.txt, which
-#'     is created by the function `update_data`.
+#'     (character) The path to which the taxonomicCoverage will be written, and
+#'     or to where taxa_map.csv is located.
 #'
 #' @return
-#'     The taxonomicCoverage EML element written to the file
+#'     The taxonomicCoverage EML as an XML object, and or written to the file
 #'     taxonomicCoverage.xml in the directory specified by path.
-#'     When creating EML for your dataset using the EMLassemblyline
-#'     (https://github.com/EDIorg/EMLassemblyline), this file is read
-#'     and the contents incorporated into the EML for this dataset.
 #'
 #' @export
 #'
 
+make_taxonomicCoverage <- function(taxa.clean, authority, authority.id, path = NULL){
 
-make_taxonomicCoverage <- function(path){
+  data <- unname(get_classification(taxa.clean, authority, authority.id, path))
 
-  # message('Checking arguments.')
-  #
-  # if (missing(path)){
-  #   stop('Input argument "path" is missing! Specify the path to your dataset working directory.')
-  # }
-  #
-  # # Validate path
-  #
-  # validate_path(path)
-  #
-  # # Read in taxon.txt
-  #
-  # if (!file.exists(paste(path, "/", "taxon.txt", sep = ""))){
-  #   stop('taxon.txt does not exist! Please create this file or remove the time stamp from the version you would like to use with your data.')
-  # }
-  #
-  # taxon <- utils::read.table(paste(path, "/", "taxon.txt", sep = ""),
-  #                     header = T,
-  #                     sep = "\t",
-  #                     as.is = T,
-  #                     na.strings = "NA")
-  #
-  # # Create EML taxonomicCoverage element --------------------------------------
-  #
-  # ids <- taxon$authority_taxon_id[stats::complete.cases(taxon$authority_taxon_id)]
-  #
-  # classifications <- classification(x = ids, db = "itis")
-  #
-  # # build up taxonomic system (can there be more than one?)
-  #
-  # message('Making <taxonomicCoverage>')
-  #
-  # ranks <- classifications[[1]]$rank
-  # df <- matrix(NA, nrow = length(classifications), ncol = length(ranks))
-  # df <- as.data.frame(df)
-  # colnames(df) <- ranks
-  # df <- data.frame(lapply(df, as.character), stringsAsFactors = F)
-  # suppressWarnings(for (i in 1:length(classifications)){
-  #   use_i <- match(ranks, classifications[[i]]$rank)
-  #   if (sum(is.na(use_i)) > 0){
-  #     use_i2 <- is.na(use_i)
-  #     use_i <- use_i[!use_i2]
-  #
-  #     if (max(use_i, na.rm = T) > length(ranks)){
-  #       use_i4 <- use_i > length(ranks)
-  #       use_i <- use_i[!use_i4]
-  #       df[i, use_i] <- classifications[[i]]$name[use_i]
-  #     } else {
-  #       use_i2 <- is.na(use_i)
-  #       use_i <- use_i[!use_i2]
-  #       df[i, use_i] <- classifications[[i]]$name[use_i]
-  #     }
-  #
-  #   } else {
-  #     df[i, use_i] <- classifications[[i]]$name[use_i]
-  #   }
-  # })
-  #
-  # taxon_coverage <- set_taxonomicCoverage(df)
-  #
-  # # Write to file
-  #
-  # message('Writing taxonomicCoverage.xml')
-  #
-  # write_eml(eml = taxon_coverage,
-  #           file = paste(path, "/", "taxonomicCoverage.xml", sep = ""))
-  #
-  # message('Done.')
+  dataframe_2_taxclass <- function(x){
+    if (('name' %in% colnames(x)) & ('rank' %in% colnames(x))){
+      df <- dplyr::select(x, name, rank)
+      df <- as.data.frame(t(data.frame(df$name)))
+      colnames(df) <- x$rank
+      taxcov <- EML::set_taxonomicCoverage(df)
+      taxcov@taxonomicClassification[[1]]
+    }
+  }
+
+  taxclass <- lapply(data, dataframe_2_taxclass)
+  taxcov <- new('taxonomicCoverage')
+  taxcov@taxonomicClassification <- as(taxclass,
+                                       'ListOftaxonomicClassification')
+
+  if (!is.null(path)){
+    write_eml(coverage, paste0(path, "/", "taxonomicCoverage.xml"))
+  }
+
+  taxcov
 
 }
-
-
