4ba731dfc2b6954a4390a9bccdbe0cb4dc248b1e
clnsmth <colin.smith@wisc.edu>
2018-12-18 11:12:07 -0500

    Remove path dependency

--- R/get_classification.R
+++ R/get_classification.R
@@ -35,13 +35,12 @@ get_classification <- function(taxa.clean, authority, authority.id, path = NULL)
                                         'tropicos'),
                    stringsAsFactors = F)
 
-  if (is.null(path)){
-    use_i <- match(authority, cw$human.readable)
-    output <- suppressMessages(mapply(taxize::classification,
-                                      x = authority.id,
-                                      db = cw$machine.readable[use_i]))
-    names(output) <- taxa.clean
-    output <- lapply(output, as.data.frame)
-    output
-  }
+  use_i <- match(authority, cw$human.readable)
+  use_i[is.na(use_i)] <- 2
+  output <- suppressMessages(mapply(taxize::classification,
+                                    x = authority.id,
+                                    db = cw$machine.readable[use_i]))
+  names(output) <- taxa.clean
+  output <- lapply(output, as.data.frame)
+  output
 }
