fd041ade5411809f6cd8c3a9d1c224402550c158
clnsmth <colin.smith@wisc.edu>
2018-12-23 13:03:14 -0500

    Expand test coverage

--- tests/testthat/test_resolve_sci_taxa.R
+++ tests/testthat/test_resolve_sci_taxa.R
@@ -43,64 +43,64 @@ testthat::test_that('Output table is standardized', {
   expect_equal(class(resolve_sci_taxa(path = path, data.sources = 3)),
                'data.frame')
 
-  # # COL
-  # expect_equal(colnames(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
-  #                                        data.sources = 3)),
-  #              c('index', 'taxa', 'taxa_clean', 'rank', 'authority',
-  #                'authority_id', 'score'))
-  # expect_equal(class(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
-  #                                     data.sources = 3)),
-  #              'data.frame')
-  # expect_equal(colnames(resolve_sci_taxa(path = path, data.sources = 3)),
-  #              c('taxa_raw', 'taxa_trimmed', 'taxa_replacement', 'taxa_removed',
-  #                'taxa_clean', 'rank', 'authority', 'authority_id', 'score',
-  #                'difference'))
-  # expect_equal(class(resolve_sci_taxa(path = path, data.sources = 3)),
-  #              'data.frame')
+  # COL
+  expect_equal(colnames(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
+                                         data.sources = 1)),
+               c('index', 'taxa', 'taxa_clean', 'rank', 'authority',
+                 'authority_id', 'score'))
+  expect_equal(class(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
+                                      data.sources = 1)),
+               'data.frame')
+  expect_equal(colnames(resolve_sci_taxa(path = path, data.sources = 1)),
+               c('taxa_raw', 'taxa_trimmed', 'taxa_replacement', 'taxa_removed',
+                 'taxa_clean', 'rank', 'authority', 'authority_id', 'score',
+                 'difference'))
+  expect_equal(class(resolve_sci_taxa(path = path, data.sources = 1)),
+               'data.frame')
 
-  # # WORMS
-  # expect_equal(colnames(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
-  #                                        data.sources = 3)),
-  #              c('index', 'taxa', 'taxa_clean', 'rank', 'authority',
-  #                'authority_id', 'score'))
-  # expect_equal(class(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
-  #                                     data.sources = 3)),
-  #              'data.frame')
-  # expect_equal(colnames(resolve_sci_taxa(path = path, data.sources = 3)),
-  #              c('taxa_raw', 'taxa_trimmed', 'taxa_replacement', 'taxa_removed',
-  #                'taxa_clean', 'rank', 'authority', 'authority_id', 'score',
-  #                'difference'))
-  # expect_equal(class(resolve_sci_taxa(path = path, data.sources = 3)),
-  #              'data.frame')
+  # WORMS
+  expect_equal(colnames(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
+                                         data.sources = 9)),
+               c('index', 'taxa', 'taxa_clean', 'rank', 'authority',
+                 'authority_id', 'score'))
+  expect_equal(class(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
+                                      data.sources = 9)),
+               'data.frame')
+  expect_equal(colnames(resolve_sci_taxa(path = path, data.sources = 9)),
+               c('taxa_raw', 'taxa_trimmed', 'taxa_replacement', 'taxa_removed',
+                 'taxa_clean', 'rank', 'authority', 'authority_id', 'score',
+                 'difference'))
+  expect_equal(class(resolve_sci_taxa(path = path, data.sources = 9)),
+               'data.frame')
 
-  # # GBIF
-  # expect_equal(colnames(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
-  #                                        data.sources = 3)),
-  #              c('index', 'taxa', 'taxa_clean', 'rank', 'authority',
-  #                'authority_id', 'score'))
-  # expect_equal(class(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
-  #                                     data.sources = 3)),
-  #              'data.frame')
-  # expect_equal(colnames(resolve_sci_taxa(path = path, data.sources = 3)),
-  #              c('taxa_raw', 'taxa_trimmed', 'taxa_replacement', 'taxa_removed',
-  #                'taxa_clean', 'rank', 'authority', 'authority_id', 'score',
-  #                'difference'))
-  # expect_equal(class(resolve_sci_taxa(path = path, data.sources = 3)),
-  #              'data.frame')
+  # GBIF
+  expect_equal(colnames(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
+                                         data.sources = 11)),
+               c('index', 'taxa', 'taxa_clean', 'rank', 'authority',
+                 'authority_id', 'score'))
+  expect_equal(class(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
+                                      data.sources = 11)),
+               'data.frame')
+  expect_equal(colnames(resolve_sci_taxa(path = path, data.sources = 11)),
+               c('taxa_raw', 'taxa_trimmed', 'taxa_replacement', 'taxa_removed',
+                 'taxa_clean', 'rank', 'authority', 'authority_id', 'score',
+                 'difference'))
+  expect_equal(class(resolve_sci_taxa(path = path, data.sources = 11)),
+               'data.frame')
 
-  # # Tropicos
-  # expect_equal(colnames(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
-  #                                        data.sources = 3)),
-  #              c('index', 'taxa', 'taxa_clean', 'rank', 'authority',
-  #                'authority_id', 'score'))
-  # expect_equal(class(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
-  #                                     data.sources = 3)),
-  #              'data.frame')
-  # expect_equal(colnames(resolve_sci_taxa(path = path, data.sources = 3)),
-  #              c('taxa_raw', 'taxa_trimmed', 'taxa_replacement', 'taxa_removed',
-  #                'taxa_clean', 'rank', 'authority', 'authority_id', 'score',
-  #                'difference'))
-  # expect_equal(class(resolve_sci_taxa(path = path, data.sources = 3)),
-  #              'data.frame')
+  # Tropicos
+  expect_equal(colnames(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
+                                         data.sources = 165)),
+               c('index', 'taxa', 'taxa_clean', 'rank', 'authority',
+                 'authority_id', 'score'))
+  expect_equal(class(resolve_sci_taxa(x = 'Oncorhynchus tshawytscha',
+                                      data.sources = 165)),
+               'data.frame')
+  expect_equal(colnames(resolve_sci_taxa(path = path, data.sources = 165)),
+               c('taxa_raw', 'taxa_trimmed', 'taxa_replacement', 'taxa_removed',
+                 'taxa_clean', 'rank', 'authority', 'authority_id', 'score',
+                 'difference'))
+  expect_equal(class(resolve_sci_taxa(path = path, data.sources = 165)),
+               'data.frame')
 
 })
