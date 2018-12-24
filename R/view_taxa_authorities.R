c5743cb92266c991cc54d825ba7e4d0868ac337a
clnsmth <colin.smith@wisc.edu>
2018-12-24 12:39:58 -0500

    Expand test coverage

--- R/view_taxa_authorities.R
+++ R/view_taxa_authorities.R
@@ -11,8 +11,8 @@
 #'     `resolve_common`.
 #'
 #' @return
-#'     A data frame and view of taxonomic authorities and corresponding
-#'     identifiers supported by `resolve_taxa` and `resolve_common`.
+#'     (data frame) Taxonomic authorities and corresponding identifiers
+#'     supported by `resolve_taxa` and `resolve_common`.
 #'
 #' @export
 #'
@@ -35,10 +35,6 @@ view_taxa_authorities <- function(){
   gnr_list[use_i, 'resolve_taxa'] <- 'supported'
   gnr_list[use_i, 'resolve_common'] <- 'supported'
 
-  # # Can not query via get_ids_.R. Key required.
-  # use_i <- gnr_list[ , 'title'] == 'NCBI'
-  # gnr_list[use_i, 'return_to_user'] <- 'National Center for Biotchnology Information (NCBI)'
-
   # Difficulty securing accurate ranks
   use_i <- gnr_list[ , 'title'] == 'EOL'
   gnr_list[use_i, 'return_to_user'] <- 'Encyclopedia of Life (EOL)'
@@ -60,14 +56,6 @@ view_taxa_authorities <- function(){
   gnr_list[use_i, 'resolve_taxa'] <- 'supported'
   gnr_list[use_i, 'resolve_common'] <- 'not supported'
 
-  # # Authentication required
-  # use_i <- gnr_list[ , 'title'] == 'IUCN Red List of Threatened Species'
-  # gnr_list[use_i, 'return_to_user'] <- 'International Union for Conservation of Nature and Natural Resources (IUCN) Red List of Threatened Species'
-
-  # # No support for this resource
-  # use_i <- gnr_list[ , 'title'] == 'Open Tree of Life Reference Taxonomy'
-  # gnr_list[use_i, 'return_to_user'] <- 'Open Tree of Life Reference Taxonomy'
-
   use_i <- gnr_list[ , 'title'] == 'World Register of Marine Species'
   gnr_list[use_i, 'return_to_user'] <- 'World Register of Marine Species (WORMS)'
   gnr_list[use_i, 'resolve_taxa'] <- 'supported'
@@ -76,8 +64,6 @@ view_taxa_authorities <- function(){
   taxonomic_authorities <- gnr_list[stats::complete.cases(gnr_list), c('id', 'return_to_user', 'resolve_taxa', 'resolve_common')]
   colnames(taxonomic_authorities) <- c('id', 'authority', 'resolve_sci_taxa', 'resolve_comm_taxa')
   rownames(taxonomic_authorities) <- c()
-
-  utils::View(taxonomic_authorities)
 
   taxonomic_authorities
 
