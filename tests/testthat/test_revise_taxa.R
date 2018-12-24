07ca80a677b4dfe775c859820f8c5b35c0a6b584
clnsmth <colin.smith@wisc.edu>
2018-12-24 12:24:30 -0500

    Expand test coverage

--- tests/testthat/test_revise_taxa.R
+++ tests/testthat/test_revise_taxa.R
@@ -3,33 +3,35 @@ library(taxonomyCleanr)
 
 # Parameterize ----------------------------------------------------------------
 
-# data <- utils::read.table(
-#   system.file('taxa_map.csv', package = 'taxonomyCleanr'),
-#   header = TRUE,
-#   sep = ',',
-#   as.is = TRUE
-# )
-# data <- data[!is.na(data$authority), ]
-# data <- data[data$rank == 'Common', ]
-# data <- data[data$authority == 'ITIS', ]
-#
-path <- system.file('test_data.txt', package = 'taxonomyCleanr')
-path <- substr(path, 1, nchar(path) - 14)
+data <- utils::read.table(
+  system.file('test_data.txt', package = 'taxonomyCleanr'),
+  header = TRUE,
+  sep = '\t',
+  as.is = TRUE
+)
+
+path <- system.file('taxa_map.csv', package = 'taxonomyCleanr')
+path <- substr(path, 1, nchar(path) - 13)
 
 # Tests -----------------------------------------------------------------------
 
-# testthat::test_that('Expect errors', {
-#
-#   expect_error(revise_taxa(x = data))
-#   expect_error(revise_taxa(path = path))
-#   expect_error(revise_taxa(x = data, col = 'Species'))
-#   expect_error(revise_taxa(x = data, col = 'Species', sep = ','))
-#
-# })
+testthat::test_that('Expect errors', {
 
-# testthat::test_that('Output table is standardized', {
-#
-#   #revise_taxa(path = path, data.sources = 3)
-#   #revise_taxa(x = 'Yellow Perch', data.sources = 3)
-#
-# })
+  expect_error(revise_taxa(x = data))
+  expect_error(revise_taxa(path = path))
+  expect_error(revise_taxa(x = data, col = 'Species'))
+  expect_error(revise_taxa(x = data, col = 'Species', sep = ','))
+
+})
+
+testthat::test_that('Output table is standardized', {
+
+  expect_equal(class(revise_taxa(path = path, x = data, col = 'Species',
+                                 sep = ',')),
+               'data.frame')
+  expect_equal(colnames(revise_taxa(path = path, x = data,
+                                    col = 'Species', sep = ',')),
+               c('Year', 'Sample_Date', 'Plot', 'Heat_Treatment', 'Species',
+                 'Mass', 'taxa_clean', 'taxa_rank', 'taxa_authority',
+                 'taxa_authority_id'))
+})
