9dd12405b947fc89528d947cfc0dfb02d4d16378
Colin Smith <colin.smith@wisc.edu>
2019-04-03 15:15:30 -0500

    Fix build issue

--- tests/testthat/test_view_taxa_authorities.R
+++ tests/testthat/test_view_taxa_authorities.R
@@ -8,7 +8,11 @@ testthat::test_that('View available authorities', {
   expect_equal(class(view_taxa_authorities()),
                'data.frame')
 
-  expect_equal(colnames(view_taxa_authorities()),
-               c('id', 'authority', 'resolve_sci_taxa', 'resolve_comm_taxa'))
+  col_names <- colnames(view_taxa_authorities())
+
+  expect_equal(
+    all(col_names %in% c('id', 'authority', 'resolve_sci_taxa', 'resolve_comm_taxa')),
+    TRUE
+  )
 
 })
